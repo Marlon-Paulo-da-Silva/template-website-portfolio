1e172f1b3e21cea1c6da55d0055eb7bf5645ed6a
Marlon Paulo <marlon.paulo.silva@outlook.com>
2023-03-17 14:39:53 -0300

    Adicionado a Typed.JS

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ menuBtn.addEventListener('click', function(e){
   document.querySelector('body').classList.toggle('mobile-nav-active')
   this.classList.toggle('fa-xmark');
 })
+
+// Typing Effect
+
+let typed = new Typed('.auto-input', {
+  strings: ['Back-End Developer!', 'Back-End Developer!', 'Freelancer!', 'Youtuber!'],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true
+});
