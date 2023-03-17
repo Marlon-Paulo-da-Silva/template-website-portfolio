2f4eae88971695e26ad218747f87ee5eebae2d18
Marlon Paulo <marlon.paulo.silva@outlook.com>
2023-03-17 09:58:22 -0300

    Adicionando botão Toggle para abrir o cabeçalho de navegação

---
+++ script.js
@@ -0,0 +1,8 @@
+// Header Toggle
+
+let menuBtn = document.getElementById('menuBtn');
+
+menuBtn.addEventListener('click', function(e){
+  document.querySelector('body').classList.toggle('mobile-nav-active')
+  this.classList.toggle('fa-xmark');
+})
